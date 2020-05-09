ef937657592c178beadf255f457bb14b31807ec4
alexpetergill <gilla@ymail.com>
2020-05-09 20:50:25 +0100

    Test change to siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Alex Peter Gill | Frontend Developer`,
+    description: `Frontend Developer Alex Peter Gill`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
